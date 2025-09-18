a1e27dd93940d95c58e11bd4ef9152030801ae65
Pratik Vikram Davare <pratikvikramdavare@Pratiks-MacBook-Air-2.local>
2025-09-18 19:42:05 +0530

    Backend

---
+++ Backend/src/test/java/com/example/Credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
